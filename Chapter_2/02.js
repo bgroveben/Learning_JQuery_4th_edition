ab919c5eb804d72222c8f4fd4e50293a881b4364
bgrove1974 <bgroveben@gmail.com>
2016-02-23 03:16:35 -0800

    Attribute selectors

--- Chapter_2/02.js
+++ Chapter_2/02.js
@@ -4,3 +4,17 @@
 // page or follow along with the examples in the book.
 //
 // See README.txt for more information.
+
+/* Below we use the child combinator (>) to add the horizontal class to all of the top-level items only.
+In effect, the selector inside the $() function is saying "Find each list item(li) that is a child(>)
+of the element with an ID of selected-plays(#selected-plays)."
+*/
+$(document).ready(function() {
+  $('#selected-plays > li').addClass('horizontal');
+// One way to select all sub-level (everything not top-level) items is to use a negation pseudo-class.
+  $('#selected-plays li:not(.horizontal)').addClass('sub-level');
+/* The above line selects every <li> that:
+  Is a descendant of the element with an ID of selected-plays(#selected-plays)
+  Does not have a class of horizontal (:not(.horizontal))
+*/
+});
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Copyright © 2013 Packt Publishing  ISBN 978-1-78216-314-5  Published by Packt P
 
 Thanks Packt!
 
-Chapter 2 "CSS selectors" pp 107  
+Chapter 2 "Attribute selectors" pp 116  
