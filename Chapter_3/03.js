c802938d925451d5022bb99acc7ae410061d7c96
bgrove1974 <bgroveben@gmail.com>
2016-03-13 01:38:41 -0800

    Event Propagation

--- Chapter_3/03.js
+++ Chapter_3/03.js
@@ -23,10 +23,15 @@ jQuery(function($) {
   // Code that uses $.
 });
 */
+$(document).ready(function() {
+  $('#switcher h3').click(function() {
+    $('#switcher button').toggleClass('hidden');
+  });
+});
 
 $(document).ready(function() {
   $('#switcher-default').addClass('selected');
-  $('#switcher button').on('click', function() {
+  $('#switcher button').click(function() {
     var bodyClass = this.id.split('-')[1];
     $('body').removeClass().addClass(bodyClass);
     $('#switcher button').removeClass('selected');
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Copyright © 2013 Packt Publishing  ISBN 978-1-78216-314-5  Published by Packt P
 
 Thanks Packt!
 
-Chapter 3 pp 182 Shorthand events
+Chapter 3 pp 186 Event Propagation
