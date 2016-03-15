28f7048c1fd29e6de4f0c0971bb6ef45ce3f0ff7
bgrove1974 <bgroveben@gmail.com>
2016-03-15 03:26:56 -0700

    Event targets

--- Chapter_3/03.js
+++ Chapter_3/03.js
@@ -32,16 +32,19 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-  $('#switcher h3').click(function() {
+  $('#switcher').click(function(event) {
     $('#switcher button').toggleClass('hidden');
   });
 });
 
 $(document).ready(function() {
   $('#switcher-default').addClass('selected');
+
   $('#switcher button').click(function() {
     var bodyClass = this.id.split('-')[1];
+
     $('body').removeClass().addClass(bodyClass);
+
     $('#switcher button').removeClass('selected');
     $(this).addClass('selected');
   });
