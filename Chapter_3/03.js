591800d72688ba6f5d609fc72de21ad2467a4416
bgrove1974 <bgroveben@gmail.com>
2016-03-17 15:01:31 -0700

    Preventing default actions

--- Chapter_3/03.js
+++ Chapter_3/03.js
@@ -23,6 +23,7 @@ jQuery(function($) {
   // Code that uses $.
 });
 */
+
 $(document).ready(function() {
   $('#switcher h3').hover(function() {
     $(this).addClass('hover');
@@ -33,21 +34,20 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $('#switcher').click(function(event) {
-    if (event.target == this) {
-      $('#switcher button').toggleClass('hidden');
-    }
+    $('#switcher button').toggleClass('hidden');
   });
 });
 
 $(document).ready(function() {
   $('#switcher-default').addClass('selected');
 
-  $('#switcher button').click(function() {
+  $('#switcher button').click(function(event) {
     var bodyClass = this.id.split('-')[1];
 
     $('body').removeClass().addClass(bodyClass);
 
     $('#switcher button').removeClass('selected');
     $(this).addClass('selected');
+    event.stopPropagation();
   });
 });
