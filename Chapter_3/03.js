002099515dc74d58e438f23e91b1e226a1dfca68
bgrove1974 <bgroveben@gmail.com>
2016-05-21 00:42:42 -0700

    Event delegation as a way of making bubbling useful

--- Chapter_3/03.js
+++ Chapter_3/03.js
@@ -41,13 +41,15 @@ $(document).ready(function() {
 $(document).ready(function() {
   $('#switcher-default').addClass('selected');
 
-  $('#switcher button').click(function(event) {
-    var bodyClass = this.id.split('-')[1];
+  $('#switcher').click(function(event) {
+    if ($(event.target).is('button')) {
+      var bodyClass = event.target.id.split('-')[1];
 
-    $('body').removeClass().addClass(bodyClass);
+      $('body').removeClass().addClass(bodyClass);
 
-    $('#switcher button').removeClass('selected');
-    $(this).addClass('selected');
-    event.stopPropagation();
+      $('#switcher button').removeClass('selected');
+      $(event.target).addClass('selected');
+      event.stopPropagation();
+    }
   });
 });
