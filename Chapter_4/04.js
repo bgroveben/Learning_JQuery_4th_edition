4452a9657d713e9c087ab9a2c3d15711ba35fd3d
bgrove1974 <bgroveben@gmail.com>
2016-05-27 01:35:36 -0700

    Create variables to store jQuery objects

--- Chapter_4/04.js
+++ Chapter_4/04.js
@@ -2,7 +2,8 @@
 // Refer to the previous chapters if you get confused.
 
 $(document).ready(function() {
+  var $speech = $('div.speech');
   $('#switcher-large').click(function() {
-
+    var num = parseFloat($speech.css('fontSize'));
   });
 });
