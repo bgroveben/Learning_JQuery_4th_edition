61cf0bfba6b2da75669c51399005a19b0bedf234
bgrove1974 <bgroveben@gmail.com>
2016-05-27 01:53:00 -0700

    Decrease font size by 40% when the 'Smaller' button is clicked

--- Chapter_4/04.js
+++ Chapter_4/04.js
@@ -3,11 +3,16 @@
 
 $(document).ready(function() {
   var $speech = $('div.speech');
-  $('#switcher-large').click(function() {
+  $('#switcher button').click(function() {
     // use parseFloat to get the font size property's numeric value only and exclude the 'px' at the end
     var num = parseFloat($speech.css('fontSize'));
-    // increase font size by 40%
-    num *= 1.4;
+    if (this.id == 'switcher-large') {
+      // increase font size by 40%
+      num *= 1.4;
+    } else if (this.id == 'switcher-small') {
+      // decrease font-size by 40%
+      num /= 1.4;
+    }
     $speech.css('fontSize', num + 'px');
     // display font size in console
     console.log($('div.speech').css('fontSize')); // ('font-size') also works
