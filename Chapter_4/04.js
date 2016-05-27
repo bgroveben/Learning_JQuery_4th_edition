f9f85dd0374da58a9e37578f3a3c74706f257f57
bgrove1974 <bgroveben@gmail.com>
2016-05-27 01:44:53 -0700

    Increase the font size by 40% when the 'Bigger' button is clicked

--- Chapter_4/04.js
+++ Chapter_4/04.js
@@ -4,6 +4,12 @@
 $(document).ready(function() {
   var $speech = $('div.speech');
   $('#switcher-large').click(function() {
+    // use parseFloat to get the font size property's numeric value only and exclude the 'px' at the end
     var num = parseFloat($speech.css('fontSize'));
+    // increase font size by 40%
+    num *= 1.4;
+    $speech.css('fontSize', num + 'px');
+    // display font size in console
+    console.log($('div.speech').css('fontSize')); // ('font-size') also works
   });
 });
